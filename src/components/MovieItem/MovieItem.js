13c81c9de680e639abd082da2a8b0bb84438120f
Jayden Thunberg <jlthunberg@gmail.com>
2020-05-17 12:21:25 -0500

    card not working. back to list

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 class MovieItem extends Component {
     componentDidMount() {
@@ -7,10 +12,16 @@ class MovieItem extends Component {
     // Renders the entire MovieItem on the DOM
     render() {
         return (
-            <div className="MovieItem">
-                <img src={this.props.movie.poster} />
+            <div>
+                <Card>
+                        <CardMedia
+                        image={this.props.movie.poster}
+                        />
+                </Card>
+                <h2>{this.props.movie.title}</h2>
+                <img src={this.props.movie.poster} alt={this.props.movie.title} />
                 <p>{this.props.movie.description}</p>
-            </div>
+            </div> 
         );
     }
 }
