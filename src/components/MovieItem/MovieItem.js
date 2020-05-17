01dae74c0926a927d4d0e6482bd84faadca98a90
Jayden Thunberg <jlthunberg@gmail.com>
2020-05-17 08:05:27 -0500

    basic get route and components

---
+++ src/components/MovieItem/MovieItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MovieItem extends Component {
+  // Renders the entire MovieItem on the DOM
+  render() {
+    return (
+      <div className="MovieItem">
+        <p>Template</p>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
---
+++ src/components/MovieList/MovieList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  // Renders the entire MovieList on the DOM
+  render() {
+    return (
+      <div className="MovieList">
+        <p>Template</p>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
